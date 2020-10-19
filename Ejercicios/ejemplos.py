07478f5cf9025885ed53ba086ff90de9ad558b2c
Simon Jose Lopez Solis - SAN_OFPP <simon.lopez@aliar.com.co>
2020-10-18 23:31:34 -0500

    desplazamiento binario

---
+++ Ejercicios/ejemplos.py
@@ -0,0 +1,28 @@
+#Elementos de comparación(| , & , ~(virgulilla) , ^(compuerta xor)
+a = 37
+
+b = 36
+    
+   if a & b:
+
+       print("mi bit esta listo")
+
+
+
+"""
+#Desplazamiento binario <<izquierda o >>derecha; lo que hace es que en el sistema binario desplaza los valores activos a la derecha o a la izquierda
+var = 20
+varRight = var >> 1
+varLeft = var << 1
+print(var, varLeft, varRight)
+
+"""
+
+#EjemploFuncionmapa
+#EjemploFunciónEnumerar
+#EjemploFuncionZip
+
+
+
+
+
